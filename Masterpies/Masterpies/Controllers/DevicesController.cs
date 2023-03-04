7a48b55805c4256a97cde06150c30ff79e1f44bb
33-sohaibrousan <sohaibalrousan99@gmail.com>
2023-03-04 22:20:51 +0300

    master

---
+++ Masterpies/Masterpies/Controllers/DevicesController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Masterpies.Models;
+
+namespace Masterpies.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private MasterPieseEntities db = new MasterPieseEntities();
+
+        // GET: Devices
+        public ActionResult Index()
+        {
+            return View(db.Devices.ToList());
+        }
+
+        // GET: Devices/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DeviceID,DeviceName,Description,DeviceImage")] Device device , HttpPostedFileBase DeviceImage)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DeviceImage != null)
+                {
+                    if (!DeviceImage.ContentType.ToLower().StartsWith("image/"))
+                    {
+                        ModelState.AddModelError("", "file uploaded is not an image");
+                        return View(DeviceImage);
+                    }
+                    string folderPath = Server.MapPath("~/Content/Images"); 
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    string fileName = Path.GetFileName(DeviceImage.FileName);
+                    string path = Path.Combine(folderPath, fileName);
+                    DeviceImage.SaveAs(path);
+                    device.DeviceImage = "../Content/Images/" + fileName;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Please upload an image.");
+                    return View(device);
+                }
+                db.Devices.Add(device);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(device);
+        }
+
+        // GET: Devices/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Device device = db.Devices.Find(id);
+            Session["img"] = device.DeviceImage;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DeviceID,DeviceName,Description,DeviceImage")] Device device , HttpPostedFileBase DeviceImage)
+        {
+            device.DeviceImage = Session["img"].ToString();
+            if (ModelState.IsValid)
+            {
+
+                if (DeviceImage != null)
+                {
+                    if (!DeviceImage.ContentType.ToLower().StartsWith("image/"))
+                    {
+                        ModelState.AddModelError("", "file uploaded is not an image");
+                        return View(DeviceImage);
+                    }
+                    string folderPath = Server.MapPath("~/Content/Images");
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    string fileName = Path.GetFileName(DeviceImage.FileName);
+                    string path = Path.Combine(folderPath, fileName);
+                    DeviceImage.SaveAs(path);
+                    device.DeviceImage = "../Content/Images/" + fileName;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Please upload an image.");
+                    return View(device);
+                }
+                db.Entry(device).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Device device = db.Devices.Find(id);
+            db.Devices.Remove(device);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
